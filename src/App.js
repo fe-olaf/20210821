47c7a2f9fa4b53795c4e9c73f5007a5d2730702d
fe-olaf <frontend.olaf@gmail.com>
2021-09-03 21:23:50 +0900

    input 추가

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react'
 import { BrowserRouter, Switch, Route } from 'react-router-dom'
 
+import ResetStyle from './components/ResetStyle'
 import TodoPage from './pages/Todo'
 import ErrorPage from './pages/Error'
 
 function App() {
   return (
     <BrowserRouter>
+      <ResetStyle />
       <Switch>
         <Route exact path="/">
           <TodoPage />
--- src/pages/Todo.js
+++ src/pages/Todo.js
@@ -1,7 +1,21 @@
 import React from 'react'
+import styled from 'styled-components'
+
+import Input from '../components/todo/Input'
+
+const Container = styled.div`
+  background: #fff;
+  margin: 130px 0 40px 0;
+  position: relative;
+  box-shadow: 0 2px 4px 0 rgba(0, 0, 0, 0.2), 0 25px 50px 0 rgba(0, 0, 0, 0.1);
+`
 
 function TodoPage() {
-  return <div>Todo</div>
+  return (
+    <Container>
+      <Input />
+    </Container>
+  )
 }
 
 export default TodoPage
