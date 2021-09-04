ec3741aa804d3cfe66cbcaa5989486c23122b5d7
fe-olaf <frontend.olaf@gmail.com>
2021-09-04 10:15:16 +0900

    todo 기본 틀 컴포넌트 추가

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter, Switch, Route } from 'react-router-dom'
 
 import TodoPage from './pages/Todo'
 import ErrorPage from './pages/Error'
+import ResetStyles from './components/ResetStyles'
 
 const ROUTES = {
   TODO: '/',
@@ -11,21 +12,27 @@ const ROUTES = {
 
 /**
  * 컴포넌트들의 최상위 컴포넌트
+ *
+ * <div />
+ * <div />
  */
 function App() {
   return (
-    <BrowserRouter>
-      <Switch>
-        {/* / 나 /todo 모두 / */}
-        <Route exact path={ROUTES.TODO}>
-          <TodoPage />
-        </Route>
-        {/* 그 외에 정의되지 않은 페이지는 error page  */}
-        <Route>
-          <ErrorPage />
-        </Route>
-      </Switch>
-    </BrowserRouter>
+    <>
+      <ResetStyles />
+      <BrowserRouter>
+        <Switch>
+          {/* / 나 /todo 모두 / */}
+          <Route exact path={ROUTES.TODO}>
+            <TodoPage />
+          </Route>
+          {/* 그 외에 정의되지 않은 페이지는 error page  */}
+          <Route>
+            <ErrorPage />
+          </Route>
+        </Switch>
+      </BrowserRouter>
+    </>
   )
 }
 
